3c2fb7b8111ec406e555de2a389a248edce784c3
Mithilesh Kumar Singh <76808844+shadowfightbeast@users.noreply.github.com>
2022-05-25 11:15:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase  from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyBY8e5ndRC1fIOA6FDcQMPa80zIdJXM3Gc",
-    authDomain: "whatsapp-clone-aebfb.firebaseapp.com",
-    projectId: "whatsapp-clone-aebfb",
-    storageBucket: "whatsapp-clone-aebfb.appspot.com",
-    messagingSenderId: "108557410849",
-    appId: "1:108557410849:web:78140a3a8a3c114af5f960",
-    measurementId: "G-MYBXTK8F6D"
+   //add your own  firebaseconfig detail
   };
 
 const firebaseApp=firebase.initializeApp(firebaseConfig);
